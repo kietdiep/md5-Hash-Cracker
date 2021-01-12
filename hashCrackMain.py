79388f630c5de0e0c48999c9dda0a6d078f10fca
Chabo17 <cbott001@ucr.edu>
2021-01-11 16:21:32 -0800

    loop

--- hashCrackMain.py
+++ hashCrackMain.py
@@ -12,8 +12,29 @@ class ChadNKietOnCrack:
     def get_intermediate(insert params here):
         pass
 
-    def loop(insert params here):
-        pass
+    def loop(password, salt, h):
+        for i in range (1000):
+            tmp =""
+            if i % 2:
+                tmp += password
+            else:
+                tmp += h
+
+            if (i % 3)==0:
+                tmp += salt
+
+            if (i % 7)==0:
+                tmp += password
+
+            if i % 2:
+                tmp += h
+            else:
+                tmp += password
+
+            fin = hashlib.md5(h).digest()
+
+        return fin
+
 
     def get_bytes(insert params here):
         pass
